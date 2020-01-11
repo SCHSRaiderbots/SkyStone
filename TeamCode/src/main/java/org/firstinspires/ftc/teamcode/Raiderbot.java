2592126d142fd1bcf7feaa054f4e13829ad2f447
anishapv <43125372+anishapv@users.noreply.github.com>
2020-01-11 10:56:37 -0800

    cleaning master branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Raiderbot.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import static org.firstinspires.ftc.teamcode.SCHSConstants.*;
-
-public class Raiderbot {
-
-}
