e6a34262a78853096b796beeaf6816f82d3b9a2c
anishapv <43125372+anishapv@users.noreply.github.com>
2020-01-14 21:23:11 -0800

    Update SCHSController.java
    Added break under test 1_5 switch.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SCHSController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SCHSController.java
@@ -163,6 +163,7 @@ public class SCHSController extends OpMode {
                     startPath(testPathRun);
                     newState(State.STATE_TEST_2_INITIAL);
                 }
+                break;
 
 
             case STATE_TEST_2_INITIAL:
